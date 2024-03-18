0eb66ff8f4f5a400cbb7c8dd485f0e49016e7488
Dmytro Maniuta <ip722428@gmail.com>
2024-03-18 20:28:40 +0200

    started hw-1

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import "./App.css";
+import Description from "./description/Description";
+
+export default function App() {
+  return (
+    <>
+      <Description />
+    </>
+  );
+}
